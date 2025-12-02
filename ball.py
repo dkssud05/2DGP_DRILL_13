4caf0233672b8caa5e8d84b8c3f835781e66c79e
dkssud05 <hyeonuson51@gmail.com>
2025-12-02 17:41:13 +0900

    공이 랜덤한 위치에 생성되는 코드

--- ball.py
+++ ball.py
@@ -1,10 +1,14 @@
 from pico2d import *
+import random
 
 class Ball:
     def __init__(self):
-        self.x = x
-        self.y = y
+        self.x = random.randint(21, 1821)
+        self.y = random.randint(21, 1000)
         self.image = load_image('ball21x21.png')
 
     def update(self):
         pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
