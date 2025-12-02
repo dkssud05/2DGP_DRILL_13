0e9dca73b525dd45f97bd52cc00fff7a71820540
dkssud05 <hyeonuson51@gmail.com>
2025-12-02 17:37:12 +0900

    공 기본 함수

---
+++ ball.py
@@ -0,0 +1,10 @@
+from pico2d import *
+
+class Ball:
+    def __init__(self):
+        self.x = x
+        self.y = y
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        pass
